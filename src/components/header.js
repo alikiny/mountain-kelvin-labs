b57ac071c5c6db583c8908efd28499468e3da7c1
P1783 <p1763@student.jamk.fi>
2020-12-01 20:03:46 +0200

    Update to fetch sitename from API

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import { SITEKEY } from '../web-socket/socket'
+const axios = require('axios');
 
+const Header = () => {
 
+    const [siteName, setName]= useState('')
+    const fetchData = () => {
+        axios.get(`https://api.mountkelvin.com/v1/site/${SITEKEY}`)
+            .then(res => {setName(res.data.name)})
+            .catch(e => console.log(e))
+    }
 
-const Header = () => (
-    <div>
-        <div className="h1">
-        Mount Kelvin Labs
+    useEffect(()=>{
+        fetchData()
+    })
+
+    return (
+        <div>
+            <div className="h1">
+                {siteName}
+            </div>
         </div>
-    </div>
-)
+    )
+}
 
 
 export default Header
